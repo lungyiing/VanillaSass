bc2af8ba9330d3b557d409c8b27436efd657384d
Chris Ireland <ireland63@googlemail.com>
2013-09-01 15:59:02 +0100

    Policy Change : Live websites should always used compressed css

--- compile-min.bat
+++
@@ -1,1 +0,0 @@
-sass --watch style.scss:output/style.min.css style.scss:output/custom.min.css --style compressed
--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 sass --watch style.scss:output/style.css style.scss:output/custom.css --style compressed
